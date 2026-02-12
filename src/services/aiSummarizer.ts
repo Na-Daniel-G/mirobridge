c5398bd643880f158dc519ec9e34b58ec8a81347
Daniel Gabud <b2029dsgabud@pshs.edu.ph>
2026-02-12 18:10:26 +0800

    Add AI Summarization class in aiSummarizer.ts

---
+++ src/services/aiSummarizer.ts
@@ -0,0 +1,12 @@
+class AISummarizer {
+    constructor() {
+        // initialization code
+    }
+
+    summarize(text) {
+        // logic to summarize the text
+        return summarizedText;
+    }
+}
+
+export default AISummarizer;
